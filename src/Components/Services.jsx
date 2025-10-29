d80dde4e05fca1b917d5040f06fe483df9b6fc06
heytechie <tanishk.23bai10933@vitbhopal.ac.in>
2025-10-29 18:31:21 +0530

    extended Services.jsx

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -76,6 +76,28 @@ const Services = () => {
                   The world's most powerful Al photo and video art generation
                   engine-What will you create?
                 </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient  md:w-[4.5rem] md:h-[4.5rem] "
+                          : " flex w-10 h-10 bg-n-6 md:w-15 md:h-15 "
+                      }`}
+                    >
+                      <div
+                        className={`${
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }`}
+                      >
+                        <img src={item} alt={item} width={24} height={24} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
